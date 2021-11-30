52932862beba45261f7fced2e469f79a273433e9
Dmitriy <devilins24@gmail.com>
2021-11-30 22:15:18 +0200

    add slider steps, colors change, style v0.3

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,38 +1,85 @@
-import { useEffect, useRef } from "react";
+import { useEffect, useRef, useState } from "react";
 import check from "../images/icon-check.svg";
 
 const Card = () => {
   const rangeRef = useRef();
+  const [slideValue, setSlideValue] = useState(2);
+  const [billingType, setBillingType] = useState("monthly");
+
+  const dataObj = [
+    {
+      pageViews: "10K",
+      price: 8,
+      priceDisc: 8 - 8 * 0.25,
+    },
+    {
+      pageViews: "50K",
+      price: 12,
+      priceDisc: 12 - 12 * 0.25,
+    },
+    {
+      pageViews: "100K",
+      price: 16,
+      priceDisc: 16 - 16 * 0.25,
+    },
+    {
+      pageViews: "500K",
+      price: 24,
+      priceDisc: 24 - 24 * 0.25,
+    },
+    {
+      pageViews: "1M",
+      price: 36,
+      priceDisc: 36 - 36 * 0.25,
+    },
+  ];
+
+  const toggleBillingType = () => {
+    if (billingType === "monthly") {
+      setBillingType("yearly");
+    } else if (billingType === "yearly") {
+      setBillingType("monthly");
+    }
+  };
+
+  const handleChange = (e) => {
+    e.target.style.setProperty("--min", e.target.min === "" ? "0" : e.target.min);
+    e.target.style.setProperty("--max", e.target.max === "" ? "100" : e.target.max);
+    e.target.style.setProperty("--value", e.target.value);
+    const rangeValue = rangeRef.current;
+    setSlideValue(rangeValue.value);
+  };
 
   useEffect(() => {
     const rangeValue = rangeRef.current;
     rangeValue.style.setProperty("--min", rangeValue.min === "" ? "0" : rangeValue.min);
     rangeValue.style.setProperty("--max", rangeValue.max === "" ? "100" : rangeValue.max);
     rangeValue.style.setProperty("--value", rangeValue.value);
+    setSlideValue(rangeValue.value);
   }, []);
 
-  const handleChange = (e) => {
-    e.target.style.setProperty("--min", e.target.min === "" ? "0" : e.target.min);
-    e.target.style.setProperty("--max", e.target.max === "" ? "100" : e.target.max);
-    e.target.style.setProperty("--value", e.target.value);
-  };
-
   return (
-    <main className="pt-16 pb-12 px-12 bg-white flex flex-col shadow-xl rounded-3xl md:py-10 md:px-20">
+    <main className="pt-16 pb-12 px-12 bg-white flex flex-col  rounded-3xl md:py-10 md:px-20">
       <div className="page-views-wrap md:flex md:items-center md:justify-between">
-        <div className="page-views text-xl mb-5  uppercase tracking-widest font-bold md:m-0">
-          100K pageviews
+        <div className="page-views text-2xl mb-5  uppercase tracking-widest font-bold md:m-0">
+          {dataObj[slideValue].pageViews} pageviews
         </div>
         <div className="price price-desktop py-10 my-5 flex gap-4 justify-center items-center hidden md:flex md:gap-2">
-          <span className="price-num text-6xl font-extrabold ">$16.00</span> / month
+          <span className="price-num text-6xl font-extrabold ">
+            ${billingType === "monthly" ? dataObj[slideValue].price : dataObj[slideValue].priceDisc}
+            .00
+          </span>
+          / month
         </div>
       </div>
       <div className="slider pt-10 md:pt-0">
         <input
           className="styled-slider slider-progress"
           type="range"
-          min="1"
-          max="100"
+          min="0"
+          max="4"
+          step="1"
+          value={slideValue}
           name="range-input"
           id="range-input"
           onChange={(e) => handleChange(e)}
@@ -40,33 +87,35 @@ const Card = () => {
         />
       </div>
       <div className="price pt-10 my-5 flex gap-4 justify-center items-center md:py-0 md:hidden">
-        <span className="price-num text-5xl font-extrabold ">$16.00</span> / month
+        <span className="price-num text-5xl font-extrabold ">
+          {billingType === "monthly" ? dataObj[slideValue].price : dataObj[slideValue].priceDisc}
+          .00
+        </span>
+        / month
       </div>
       <div className="bill-type flex items-center justify-center my-3 py-10">
         <label>
           <p id="monthly-billing">Monthly Billing</p>
-          <input type="checkbox" role="switch" indeterminate />
-          <p id="yearly-billing">
-            Yearly Billing
-            <div className="discount font-bold rounded-full  py-1.5 px-3 md:gap-2 md:flex">
-              -25%
-              <span className="discount-text hidden md:inline-block"> discount</span>
-            </div>
-          </p>
+          <input type="checkbox" role="switch" onChange={toggleBillingType} />
+          <p id="yearly-billing">Yearly Billing</p>
+          <div className="discount font-bold rounded-full  py-1.5 px-3 md:gap-2 md:flex">
+            -25%
+            <span className="discount-text hidden md:inline-block"> discount</span>
+          </div>
         </label>
       </div>
       <div className="info border-t md:pt-14 md:pb-5 md:flex md:justify-between md:items-center">
         <ul className="py-16 text-xl	 flex flex-col gap-4 md:py-0">
           <li>
-            <img src={check} alt="" />
+            <img src={check} alt="checkmark" />
             Unlimited websites
           </li>
           <li>
-            <img src={check} alt="" />
+            <img src={check} alt="checkmark" />
             100% data ownership
           </li>
           <li>
-            <img src={check} alt="" />
+            <img src={check} alt="checkmark" />
             Email reports
           </li>
         </ul>
--- src/components/Header.js
+++ src/components/Header.js
@@ -2,7 +2,7 @@ const Header = () => {
   return (
     <header className="py-20 md:py-32 ">
       <h1 className="font-bold text-3xl py-5 md:py-8 md:text-5xl">Simple, traffic-based pricing</h1>
-      <div className="header-text-wrap leading-loose	 text-xl md:text-2xl md:flex md:justify-center md:gap-2 md:text-base">
+      <div className="header-text-wrap flex-wrap leading-loose	 text-xl md:text-2xl md:flex md:justify-center md:gap-2 md:text-base">
         <p>Sign-up for our 30-day trial. </p>
         <p>No credit card required.</p>
       </div>
